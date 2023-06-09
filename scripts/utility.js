31284c84a8ca3dade31c729c2dcb9509222b7e3c
abel3ri <abelmerete@yahoo.com>
2023-06-10 00:30:29 +0300

    Add animations for get started page

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,11 +1,25 @@
-const menuBtn = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close");
-menuBtn.addEventListener("click", () => {
-  overlay.classList.add("show");
-  closeBtn.classList.add("show");
-});
-closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("show");
-  closeBtn.classList.remove("show");
+const menuBtn = document.querySelector(".menu");
+if (menuBtn && closeBtn && overlay) {
+  menuBtn.addEventListener("click", () => {
+    overlay.classList.add("show");
+    closeBtn.classList.add("show");
+  });
+  closeBtn.addEventListener("click", () => {
+    overlay.classList.remove("show");
+    closeBtn.classList.remove("show");
+  });
+}
+
+window.addEventListener("load", (e) => {
+  const getStartedContainer = document.querySelector(".get-started-container");
+  const headingText = document.querySelector(".get-started-container h1");
+  const loginBtn = document.querySelector(".login-btn");
+  const signUpBtn = document.querySelector(".signup-btn");
+
+  console.log(headingText);
+  loginBtn.classList.add("show");
+  signUpBtn.classList.add("show");
+  headingText.classList.add("show");
 });
